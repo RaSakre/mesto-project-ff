7752cf3e4879cb001fa348c8779021e6b403a609
Ranel Nizamov <nizamov.ranel@yandex.ru>
2024-01-15 14:29:24 +0300

    second try

--- scripts/index.js
+++ scripts/index.js
@@ -7,23 +7,24 @@ const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-const createCards = function (cardData){
+const createCards = function (cardData, cardRemove){
 const cloneTemplate =  cardTemplate.cloneNode(true);
-cloneTemplate.querySelector('.card__image').src = cardData.link;
+const cardImage = cloneTemplate.querySelector('.card__image');
 cloneTemplate.querySelector('.card__image').alt = cardData.name;
 cloneTemplate.querySelector('.card__title').textContent = cardData.name;
 const deleteButton = cloneTemplate.querySelector('.card__delete-button');
-deleteButton.addEventListener('click', deleteFn);
+deleteButton.addEventListener('click', cardRemove);
+cardImage.src = cardData.link;
 return cloneTemplate
 };
 
 // @todo: Функция удаления карточки
 
-const deleteFn = (evt) => {
+const deleteCard = (evt) => {
 evt.target.closest('.card').remove()
 };
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(elem){
-	cardsList.append(createCards(elem));
+	cardsList.append(createCards(elem, deleteCard));
 });
